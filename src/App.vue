5960bc05cab87eff096612002fc2d66095e5e27a
cam71101 <david.fisher137@gmail.com>
2021-02-11 20:01:50 +0000

    Added home and back buttons

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,103 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-bind:key="data.id" v-for="data in dataItems">
+      <!-- <v-button v-bind:data="data" v-on:click="fetchData" /> -->
+      <div v-if="!time" v-bind:data="data">
+        <Button v-on:click="fetchData(data)"> {{ data }}</Button>
+      </div>
+    </div>
+    <h2 v-if="time">{{ time }}</h2>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
+
+// import Button from './components/Button';
 
 export default {
+  // NAME APPLICATION
   name: 'App',
+  // IMPORT COMPONENTS
   components: {
-    HelloWorld
-  }
-}
+    // Card,
+    // Spinner,
+    // custom tagname so it doesn't conflict with HTML
+    // 'v-header' : Header,
+    // 'v-button': Button,
+  },
+  // STATE
+  data() {
+    return {
+      dataItems: [],
+      currentArea: '',
+      time: '',
+    };
+  },
+  //USE EFFECT
+  created() {
+    axios.get('http://worldtimeapi.org/api/timezone"').then((response) => {
+      const array = [];
+      response.data.forEach((e) => {
+        if (e !== e.toUpperCase()) {
+          array.push(e.split('/')[0]);
+        }
+      });
+      let uniqueValues = [...new Set(array)];
+      this.dataItems = uniqueValues;
+    });
+  },
+  // USE MEMO
+  // computed: {
+  //   filteredList() {
+  //     return this.streamers.filter((stream) => {
+  //       return stream.name.toLowerCase().includes(this.search.toLowerCase());
+  //     });
+  //   },
+  // },
+  // FUNCTIONS GO HERE
+  methods: {
+    fetchData: function(value) {
+      // `this` inside methods points to the Vue instance
+      // `event` is the native DOM event
+
+      if (value.includes('/')) {
+        axios
+          .get('http://worldtimeapi.org/api/timezone/' + value)
+          .then((response) => {
+            let date = new Date(response.data.unixtime * 1000);
+            let hours = date.getHours();
+            var minutes = '0' + date.getMinutes();
+            var seconds = '0' + date.getSeconds();
+            var formattedTime =
+              hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+            response.data.time = formattedTime;
+            this.time = formattedTime;
+          });
+      } else {
+        axios
+          .get('http://worldtimeapi.org/api/timezone/' + value)
+          .then((response) => {
+            this.dataItems = response.data;
+          });
+      }
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: Arial;
+}
+.wrapper {
+  height: 80vh;
+  margin-left: 20%;
+  margin-right: 20%;
+  margin-top: 5rem;
 }
 </style>
---
+++ src/components/Button.vue
@@ -0,0 +1,30 @@
+<template>
+  <Button> {{ data }} </Button>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  // load in props
+  props: ['data'],
+};
+</script>
+
+<style>
+.card-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  width: 15rem;
+}
+
+.image {
+  width: 100%;
+}
+.subtitle {
+  margin: 0.6rem;
+  height: 2rem;
+}
+</style>
